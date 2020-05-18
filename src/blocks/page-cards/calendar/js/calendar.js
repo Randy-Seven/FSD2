0de735d424c233f576201a84da3723dcd4bbc202
Vital9 <vrazmakhin@gmail.com>
2020-05-19 00:37:02 +0700

    Календарь: пытаюсь подсветить диапазон между днями прибытия и выезда

--- src/blocks/page-cards/calendar/js/calendar.js
+++ src/blocks/page-cards/calendar/js/calendar.js
@@ -1,114 +1,123 @@
 let today = new Date();
+let months = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
+]
 
 function createCalendar(year, month) {
-
-    let months = [
-        'Январь',
-        'Февраль',
-        'Март',
-        'Апрель',
-        'Май',
-        'Июнь',
-        'Июль',
-        'Август',
-        'Сентябрь',
-        'Октябрь',
-        'Ноябрь',
-        'Декабрь',
-    ]
  
-    let mon = month - 1;
-    let d = new Date(year, mon);
-    let nextMonthFirstDay = 0;
-    let previousMonthLastDay = getLastDayOfMonth(year, mon - 1);
+  let mon = month - 1;
+  let d = new Date(year, mon);
+  let nextMonthFirstDay = 0;
+  let previousMonthLastDay = getLastDayOfMonth(year, mon - 1);
 
-    // Меняем месяц и год в заголовке календаря
-    let headerMonth = document.querySelector('.calendar__header-month');
-    headerMonth.innerHTML = months[mon] + ' ' + year;
+  // Меняем месяц и год в заголовке календаря
+  let headerMonth = document.querySelector('.calendar__header-month');
+  headerMonth.innerHTML = months[mon] + ' ' + year;
 
-    // Обрабатываем стрелки
-    let nextMonth = document.querySelector('.calendar__next-month_next');
-    let previousMonth = document.querySelector('.calendar__next-month_previous');
+  // Обрабатываем стрелки
+  let nextMonth = document.querySelector('.calendar__next-month_next');
+  let previousMonth = document.querySelector('.calendar__next-month_previous');
 
-    nextMonth.onclick = function() {
-        if (month == 12) {
-            createCalendar(year + 1, 1);
-        } else {
-            createCalendar(year, month + 1);
-        }
-    }
-
-    previousMonth.onclick = function() {
-        if (month == 1) {
-            createCalendar(year - 1, 12)
-        } else {
-            createCalendar(year, month - 1);
-        }
-    }
-
-    // Прячем стрелку влево если на календаре текущий год и месяц
-    if ((year == today.getFullYear()) && (month == today.getMonth() + 1)) {
-      previousMonth.style.opacity = '0';
-      previousMonth.style.pointerEvents = 'none';
-    } else {
-      previousMonth.style.opacity = '1';
-      previousMonth.style.pointerEvents = 'auto';
-    }
-
-    let table = '<table class=\'calendar__week-days\'><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr><tr>';
-    
-    // Последние числа прошлого месяца
-    // с понедельника до первого дня месяца
-    // 29 30 31 1  2  3  4
-    previousMonthLastDay -= getDay(d) - 1
-    for (let i = 0; i < getDay(d); i++) {
-      table += '<td class = \'calendar__disabled\'>' + previousMonthLastDay + '</td>';
-      previousMonthLastDay += 1;
-    }
-
-    // <td> ячейки календаря с датами
-    while (d.getMonth() == mon) {
-      // Выделяем текущую дату
-      if (d.getDate() == today.getDate() && d.getFullYear() == today.getFullYear() && today.getMonth() == mon) {
-        table += '<td class = \'calendar__current\'>' + today.getDate() + '</td>'
+  nextMonth.onclick = function() {
+      if (month == 12) {
+          createCalendar(year + 1, 1);
       } else {
-        table += '<td>' + d.getDate() + '</td>';
+          createCalendar(year, month + 1);
       }
+  }
 
-      if (getDay(d) % 7 == 6) { // Вс, последний день - перевод строки
-        table += '</tr><tr>';
+  previousMonth.onclick = function() {
+      if (month == 1) {
+          createCalendar(year - 1, 12)
+      } else {
+          createCalendar(year, month - 1);
       }
+  }
 
-      d.setDate(d.getDate() + 1);
+  // Прячем стрелку влево если на календаре текущий год и месяц
+  if ((year == today.getFullYear()) && (month == today.getMonth() + 1)) {
+    previousMonth.style.opacity = '0';
+    previousMonth.style.pointerEvents = 'none';
+  } else {
+    previousMonth.style.opacity = '1';
+    previousMonth.style.pointerEvents = 'auto';
+  }
+
+  let table = '<table class=\'calendar__week-days\'><tr><th>Пн</th><th>Вт</th><th>Ср</th><th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr><tr>';
+  
+  // Последние числа прошлого месяца
+  // с понедельника до первого дня месяца
+  // 29 30 31 1  2  3  4
+  previousMonthLastDay -= getDay(d) - 1
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td class = \'calendar__disabled\'>' + previousMonthLastDay + '</td>';
+    previousMonthLastDay += 1;
+  }
+
+  // <td> ячейки календаря с датами
+  while (d.getMonth() == mon) {
+    // Выделяем текущую дату
+    if (d.getDate() == today.getDate() && d.getFullYear() == today.getFullYear() && today.getMonth() == mon) {
+      table += '<td class = \'calendar__current\'>' + today.getDate() + '</td>'
+    } else {
+      table += '<td>' + d.getDate() + '</td>';
     }
 
-    // Добить таблицу числами следующего месяца, если нужно
-    // 29 30 31 1 2 3 4
-    if (getDay(d) != 0) {
-      for (let i = getDay(d); i < 7; i++) {
-        nextMonthFirstDay += 1
-        table += '<td class = \'calendar__disabled\'>' + nextMonthFirstDay + '</td>';
-      }
+    if (getDay(d) % 7 == 6) { // Вс, последний день - перевод строки
+      table += '</tr><tr>';
     }
 
-    // Закрыть таблицу
-    table += '</tr></table>';
+    d.setDate(d.getDate() + 1);
+  }
 
-    // Исправляем конфликт с id
-    if (window.location.toString().indexOf('landing-page.html') <= 0) {
-      calendar.innerHTML = table;
+  // Добить таблицу числами следующего месяца, если нужно
+  // 29 30 31 1 2 3 4
+  if (getDay(d) != 0) {
+    for (let i = getDay(d); i < 7; i++) {
+      nextMonthFirstDay += 1
+      table += '<td class = \'calendar__disabled\'>' + nextMonthFirstDay + '</td>';
     }
-    
-    dropCalendar.innerHTML = table;
+  }
 
-    let calendarDays = document.querySelectorAll('#dropCalendar td');
+  // Закрыть таблицу
+  table += '</tr></table>';
 
-    // Делаем недоступными дни до текущей даты
-    for (let i = 1; i < calendarDays.length; i++) {
-      if (+calendarDays[i].innerHTML < +today.getDate() && (year == today.getFullYear()) && (month == today.getMonth() + 1)) {
-          calendarDays[i].classList.add('calendar__disabled')
-      }
+  // Исправляем конфликт с id
+  if (window.location.toString().indexOf('landing-page.html') <= 0) {
+    calendar.innerHTML = table;
+  }
+  
+  dropCalendar.innerHTML = table;
+
+  let calendarDays = document.querySelectorAll('#dropCalendar td');
+
+  // Делаем недоступными дни до текущей даты
+  for (let i = 1; i < calendarDays.length; i++) {
+    if (+calendarDays[i].innerHTML < +today.getDate() && (year == today.getFullYear()) && (month == today.getMonth() + 1)) {
+        calendarDays[i].classList.add('calendar__disabled')
     }
+  }
+  
+  // Форматируем месяц
+  if (month < 10) {
+    monthAndYear = '0' + month + '.' + year 
+  } else {
+    monthAndYear = month + '.' + year
+  }
+
+  //  Возвращаем месяц и год для инициализации инпутов
+  return monthAndYear 
 }
 
   // Функция, возвращающая последнее число месяца 
--- src/blocks/page-cards/room-search-form/js/room-search-form.js
+++ src/blocks/page-cards/room-search-form/js/room-search-form.js
@@ -2,7 +2,10 @@ let roomSearchArrivalButton = document.querySelector('.room-search-form__arrival
 let roomSearchCheckoutButton = document.querySelector('.room-search-form__checkout-button')
 let dropCalendar = document.querySelector('.drop-calendar')
 let calendarApplyButton = document.querySelector('.calendar__apply-js')
-let dropCalendarDays = document.querySelector('#dropCalendar')
+let dropCalendarTable = document.querySelector('#dropCalendar')
+let selectedArrivalDay
+let selectedCheckoutDay
+let secondRowInCalendar = document.querySelector('#dropCalendar tr').nextSibling
 
 // Изначально календарь спрятан
 dropCalendar.hidden = true
@@ -10,15 +13,19 @@ dropCalendar.hidden = true
 // Открываем календарь по клику 
 roomSearchArrivalButton.onclick = function() {
     dropCalendar.hidden = false;
+    roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.5)' // Выделяем кнопку прибытия
 }
 
 roomSearchCheckoutButton.onclick = function() {
     dropCalendar.hidden = false;
+    roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.5)'
 }
 
 // Кнопка "применить" закрывает календарь
 calendarApplyButton.onclick = function() {
     dropCalendar.hidden = true;
+    roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
+    roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
 }
 
 // Также закрываем календарь при клике мимо него
@@ -36,45 +43,96 @@ document.addEventListener('click', e => {
     
     if (!its_calendar &&  dropCalendar.hidden == false) {
         dropCalendar.hidden = true;
+        roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
+        roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
     }
 
 })
 
 // Выбираем даты прибытия и выезда
-let selectedArrivalDay;
-let selectedCheckoutDay;
-let calendarDays = document.querySelectorAll('#dropCalendar td');
+dropCalendarTable.onclick = function(event) {
 
-dropCalendarDays.onclick = function(event) {
+    let calendarDays = document.querySelectorAll('#dropCalendar td')
     let target = event.target;
 
     if (target.tagName != 'TD') return;
 
-    if (!selectedArrivalDay && !selectedCheckoutDay) { // Если дни не выбраны то сначала выбираем день прибытия
+    if (!selectedArrivalDay && !selectedCheckoutDay) { // Если дни не выбраны то сначала выбираем день прибытия    
         highlightArrivalDay(target)
+        roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
+        roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.5)'
+
     } else if (selectedArrivalDay && !selectedCheckoutDay) { // Если выбран день прибытия то выбираем день выезда
+        
         if (+target.innerHTML <= +selectedArrivalDay.innerHTML) { // Если выбран день прибытия и потом выбран день до него, то это перевыбор дня прибытия
-            selectedArrivalDay.classList.remove('calendar__active')
+            selectedArrivalDay.classList.remove('calendar__arrival')
             selectedArrivalDay = false;
+
             highlightArrivalDay(target)
+
+            roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
+            roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.5)'
+
         } else {
             highlightCheckoutDay(target)
+
+            roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.5)'
+            roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
         }
+
     } else if (selectedArrivalDay && selectedCheckoutDay) { // Если оба дня выбраны то сбрасываем их и снова выбираем день прибытия
-        selectedArrivalDay.classList.remove('calendar__active');
+        selectedArrivalDay.classList.remove('calendar__arrival');
+
+        roomSearchCheckoutButton.style.borderColor = 'rgba(31, 32, 65, 0.5)';
         selectedArrivalDay = false;
-        selectedCheckoutDay.classList.remove('calendar__viezd');
+
+        selectedCheckoutDay.classList.remove('calendar__checkout');
+
+        roomSearchArrivalButton.style.borderColor = 'rgba(31, 32, 65, 0.25)'
         selectedCheckoutDay = false;
+
+        roomSearchCheckoutButton.innerHTML = 'ДД.ММ.ГГГГ'
+
         highlightArrivalDay(target)
+    }
+
+    // Инициализируем день прибытия
+    if (+selectedArrivalDay.innerHTML < 10) {
+        roomSearchArrivalButton.innerHTML = '0' + selectedArrivalDay.innerHTML + '.' + monthAndYear
+    } else {
+        roomSearchArrivalButton.innerHTML = selectedArrivalDay.innerHTML + '.' + monthAndYear
+    }
+
+    // Инициализируем день выезда
+    if (selectedCheckoutDay && (+selectedCheckoutDay.innerHTML < 10)) {
+        roomSearchCheckoutButton.innerHTML = '0' + selectedCheckoutDay.innerHTML + '.' + monthAndYear
+    } else if (selectedCheckoutDay && (selectedCheckoutDay.innerHTML >= 10)) {
+        roomSearchCheckoutButton.innerHTML = selectedCheckoutDay.innerHTML + '.' + monthAndYear
+    }
+
+    if (selectedArrivalDay) {
+        dropCalendarTable.onmouseover = function(event) { // Функция, подсвечивающая диапазон между днями выезда и прибытия
+            target = event.target;
+
+            if (target.tagName != 'TD') return;
+
+            for (let i = 1; i < calendarDays.length; i++) {
+                if ((+calendarDays[i].innerHTML < +target.innerHTML) && (+calendarDays[i].innerHTML > +selectedArrivalDay.innerHTML)) {
+                    calendarDays[i].classList.add('calendar__dayInRange')
+                }
+            }
+        }
     }
 }
 
+// Подсвечивает день прибытия
 function highlightArrivalDay(day) {
     selectedArrivalDay = day;
-    selectedArrivalDay.classList.add('calendar__active'); // подсветить новый td
+    selectedArrivalDay.classList.add('calendar__arrival'); 
 }
 
+// Подсвечивает день выезда
 function highlightCheckoutDay(day) {
     selectedCheckoutDay = day;
-    selectedCheckoutDay.classList.add('calendar__viezd'); // подсветить новый td
+    selectedCheckoutDay.classList.add('calendar__checkout'); 
 }
